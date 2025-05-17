730e032d4c81d9d65270283a8fe10cdeaba1707c
Will Harris <willharris0407@gmail.com>
2025-05-17 16:37:07 -0400

    ClientPanels: dedicated function for each message

--- src/client/robust/mod.rs
+++ src/client/robust/mod.rs
@@ -1,8 +1,9 @@
 use std::io;
 
 use crate::game::kernel::Game;
-use crate::game::{messages::*, CompanyMap};
-use crate::server::{ConnectionManager, NewConnection};
+use crate::game::tile::{FullHand, Tile};
+use crate::game::{messages::*, Company, CompanyMap};
+use crate::server::{ConnectionManager, Handshake, NewConnection};
 
 use self::chat_panel::ChatPanel;
 use self::command_buffer::CommandBuffer;
@@ -255,151 +256,37 @@ impl ClientPanels {
     {
         match dbg!(msg) {
             ServerMessage::Chat { player_name, message } => {
-                let chat = format!("<{player_name}> {message}");
-                self.chat_panel.add_message(chat.into_boxed_str());
+                self.process_chat_message(player_name, message);
             },
             ServerMessage::Join { handshake } => {
-
-                // Broadcast the message
-                let spectate_msg = if handshake.spectating { " as a spectator" } else { "" };
-                let chat = format!("JOIN: {} joined the game{spectate_msg}.", handshake.player_name);
-                self.chat_panel.add_message(chat.into_boxed_str());
-
-                // Connect the player
-                self.connections.connect(handshake).unwrap();
+                self.process_join_message(handshake);
             },
             ServerMessage::Quit { handshake } => {
-
-                // Disconnect the player
-                assert!(self.connections.disconnect(&handshake.player_name));
-
-                let chat = format!("JOIN: {} left the game.", &handshake.player_name);
-                self.chat_panel.add_message(chat.into_boxed_str());
+                self.process_quit_message(handshake);
             },
             ServerMessage::PlayerMove { action } => {
-                self.chat_panel.add_message(
-                    action.to_string().into_boxed_str()
-                );
-                self.game.update(&action);
-
-                // EDGE CASE: if the action panel is trying to produce an action
-                // but the player uses the command buffer to send the action
-                // instead, the action panel will become outdated. To fix this,
-                // we clear the action panel upon receipt of a player action.
-                self.action_panel.cancel_action();
+                self.process_player_move_message(action);
             },
-            ServerMessage::DeadTile { player_name: player, dead_tile } => {
-                let msg = format!("{player} traded in dead tile {dead_tile}.");
-                self.chat_panel.add_message(msg.into_boxed_str());
+            ServerMessage::DeadTile { player_name, dead_tile } => {
+                self.process_dead_tile_message(player_name, dead_tile);
             }
             ServerMessage::GameStart { info, initial_hand } => {
-
-                // Start a new game
-                let game = Game::start(&info).into();
-                self.board_panel.draw_board(&game);
-                let hand = initial_hand.map(|hand| hand.into());
-                self.game.start(game, hand);
-
-                // Send corresponding chat messages
-                let msg = "Game started!".to_owned().into_boxed_str();
-                self.chat_panel.add_message(msg);
-                if let Some(initial_hand) = initial_hand {
-                    let msg = format!("Your starting hand is: {}.", initial_hand).into_boxed_str();
-                    self.chat_panel.add_message(msg);
-                }
+                self.process_game_start_message(info, initial_hand);
             },
             ServerMessage::CompanyDefunct { defunct, results } => {
-                let msg = format!(
-                    "Company {defunct} has gone defunct! Here are the results:"
-                ).into_boxed_str();
-                self.chat_panel.add_message(msg);
-                for result in results.iter() {
-                    let msg = result.to_string().into_boxed_str();
-                    self.chat_panel.add_message(msg);
-                }
+                self.process_company_defunct_message(defunct, results);
             },
             ServerMessage::GameOver { reason, results } => {
-
-                // End the game and draw the lobby
-                self.game.end();
-                self.board_panel.draw_lobby(&self.connections);
-
-                let msg = format!("Game Over! {reason}. Here are the results:").into_boxed_str();
-                self.chat_panel.add_message(msg);
-                results.into_iter().for_each(|result| {
-                    let msg = format!("  [{}] {} with ${}",
-                        result.place, result.player_name, result.final_money);
-                    self.chat_panel.add_message(msg.into_boxed_str())
-                });
+                self.process_game_over_message(reason, results);
             },
             ServerMessage::Shutdown => return Ok(None),
             ServerMessage::YourTurn { request } => {
-                // Processes the request
-                match request {
-                    ActionRequest::PlayTile => self.action_panel.request_place_tile(),
-                    ActionRequest::BuyStock => {
-
-                        // Figures out which companies are available
-                        todo!();
-
-                        self.action_panel.request_buy_stock(todo!());
-                    },
-                    ActionRequest::ResolveMergeStock { defunct, into } => {
-                        self.action_panel.request_resolve_merge_stock(());
-                    },
-                }
-                let request_msg = match request {
-                    ActionRequest::PlayTile => "place a tile",
-                    ActionRequest::BuyStock => "buy stock",
-                    ActionRequest::ResolveMergeStock {
-                        defunct: _, into: _
-                    } => "resolve your stock",
-                };
-                let msg = format!("Your turn to {request_msg}!");
-                self.chat_panel.add_message(msg.into_boxed_str());
-
-                if matches!(request, ActionRequest::BuyStock) {
-
-                    // If we're receiving a buy-stock request, we're assuming
-                    // the game is already started.
-                    let game = self.game.game().unwrap();
-
-                    // SHORT CIRCUIT: if there's no stock to buy, skip buying stock
-                    let none_exist = CompanyMap::new(&()).map(|cmp, _| game.board().company_exists(cmp))
-                        .iter().all(|(_, exists)| !exists);
-                    if none_exist {
-                        return Ok(Some(Some(ClientMessage::TakingTurn(
-                            PlayerAction::BuyStock { stock: [None; 3] })))
-                        )
-                    }
-
-                    // SHORT CIRCUIT: if the player can't afford stock, then skip
-                    // buying stock
-                    let player_name = &self.game.client.player_name;
-                    let player_money = game.players().get(player_name).unwrap().money;
-                    let cant_afford = CompanyMap::new(&())
-                        .map(|cmp, _| game.board().stock_price(cmp) > player_money)
-                        .iter().all(|(_, &too_expensive)| too_expensive);
-
-                    if cant_afford {
-                        let msg = "You can't afford any stock!".to_owned().into_boxed_str();
-                        self.chat_panel.add_message(msg);
-                        return Ok(Some(Some(ClientMessage::TakingTurn(
-                            PlayerAction::BuyStock { stock: [None; 3] })))
-                        )
-                    }
+                if let Some(msg) = self.process_your_turn_message(request) {
+                    return Ok(Some(Some(msg)))
                 }
             },
             ServerMessage::TileDraw { tile } => {
-
-                // Assumes a game is in progress
-                let game = self.game.game().unwrap();
-
-                // Assumes the player's hand isn't already full
-                self.action_panel.provide_tile(tile, game.board());
-
-                let msg = format!("You drew tile {}.", tile).into_boxed_str();
-                self.chat_panel.add_message(msg);
+                self.process_tile_draw_message(tile);
             }
             ServerMessage::Invalid { reason } => {
                 self.write_error(&format!("Invalid message: {reason}")).unwrap();
@@ -408,6 +295,166 @@ impl ClientPanels {
 
         Ok(Some(None))
     }
+
+    fn process_chat_message(&mut self, player_name: Box<str>, message: Box<str>) {
+        let chat = format!("<{player_name}> {message}");
+        self.chat_panel.add_message(chat.into_boxed_str());
+    }
+
+    fn process_join_message(&mut self, handshake: Handshake) {
+
+        // Broadcast the message
+        let spectate_msg = if handshake.spectating { " as a spectator" } else { "" };
+        let chat = format!("JOIN: {} joined the game{spectate_msg}.", handshake.player_name);
+        self.chat_panel.add_message(chat.into_boxed_str());
+
+        // Connect the player
+        self.connections.connect(handshake).unwrap();
+    }
+
+
+    fn process_quit_message(&mut self, handshake: Handshake) {
+
+        // Disconnect the player
+        assert!(self.connections.disconnect(&handshake.player_name));
+
+        let chat = format!("JOIN: {} left the game.", &handshake.player_name);
+        self.chat_panel.add_message(chat.into_boxed_str());
+    }
+
+    fn process_player_move_message(&mut self, action: TaggedPlayerAction) {
+        self.chat_panel.add_message(
+            action.to_string().into_boxed_str()
+        );
+        self.game.update(&action);
+
+        // EDGE CASE: if the action panel is trying to produce an action
+        // but the player uses the command buffer to send the action
+        // instead, the action panel will become outdated. To fix this,
+        // we clear the action panel upon receipt of a player action.
+        self.action_panel.cancel_action();
+    }
+
+    fn process_dead_tile_message(&mut self, player_name: Box<str>, tile: Tile) {
+        let msg = format!("{player_name} traded in dead tile {tile}.");
+        self.chat_panel.add_message(msg.into_boxed_str());
+    }
+
+    fn process_game_start_message(&mut self, start_info: GameStart, initial_hand: Option<FullHand>) {
+
+        // Start a new game
+        let game = Game::start(&start_info).into();
+        self.board_panel.draw_board(&game);
+        let hand = initial_hand.map(|hand| hand.into());
+        self.game.start(game, hand);
+
+        // Send corresponding chat messages
+        let msg = "Game started!".to_owned().into_boxed_str();
+        self.chat_panel.add_message(msg);
+        if let Some(initial_hand) = initial_hand {
+            let msg = format!("Your starting hand is: {}.", initial_hand).into_boxed_str();
+            self.chat_panel.add_message(msg);
+        }
+    }
+
+    fn process_company_defunct_message(&mut self, defunct: Company, results: Box<[PrincipleShareholderResult]>) {
+        let msg = format!(
+            "Company {defunct} has gone defunct! Here are the results:"
+        ).into_boxed_str();
+        self.chat_panel.add_message(msg);
+        for result in results.iter() {
+            let msg = result.to_string().into_boxed_str();
+            self.chat_panel.add_message(msg);
+        }
+    }
+
+    fn process_game_over_message(&mut self, reason: GameOver, results: Box<[FinalResult]>) {
+
+        // End the game and draw the lobby
+        self.game.end();
+        self.board_panel.draw_lobby(&self.connections);
+
+        let msg = format!("Game Over! {reason}. Here are the results:").into_boxed_str();
+        self.chat_panel.add_message(msg);
+        results.into_iter().for_each(|result| {
+            let msg = format!("  [{}] {} with ${}",
+                result.place, result.player_name, result.final_money);
+            self.chat_panel.add_message(msg.into_boxed_str())
+        });
+    }
+
+    /// May return a message immediately if short-circuit logic takes place
+    /// (i.e. a forced move such as a buying stock turn with no money).
+    fn process_your_turn_message(&mut self, request: ActionRequest) -> Option<ClientMessage> {
+
+        // Processes the request
+        match request {
+            ActionRequest::PlayTile => self.action_panel.request_place_tile(),
+            ActionRequest::BuyStock => {
+
+                // Figures out which companies are available
+                todo!();
+
+                self.action_panel.request_buy_stock(todo!());
+            },
+            ActionRequest::ResolveMergeStock { defunct, into } => {
+                self.action_panel.request_resolve_merge_stock(());
+            },
+        }
+        let request_msg = match request {
+            ActionRequest::PlayTile => "place a tile",
+            ActionRequest::BuyStock => "buy stock",
+            ActionRequest::ResolveMergeStock {
+                defunct: _, into: _
+            } => "resolve your stock",
+        };
+        let msg = format!("Your turn to {request_msg}!");
+        self.chat_panel.add_message(msg.into_boxed_str());
+
+        if matches!(request, ActionRequest::BuyStock) {
+
+            // If we're receiving a buy-stock request, we're assuming
+            // the game is already started.
+            let game = self.game.game().unwrap();
+
+            // SHORT CIRCUIT: if there's no stock to buy, skip buying stock
+            let none_exist = CompanyMap::new(&()).map(|cmp, _| game.board().company_exists(cmp))
+                .iter().all(|(_, exists)| !exists);
+            if none_exist {
+                return Some(ClientMessage::TakingTurn(
+                    PlayerAction::BuyStock { stock: [None; 3] }))
+            }
+
+            // SHORT CIRCUIT: if the player can't afford stock, then skip
+            // buying stock
+            let player_name = &self.game.client.player_name;
+            let player_money = game.players().get(player_name).unwrap().money;
+            let cant_afford = CompanyMap::new(&())
+                .map(|cmp, _| game.board().stock_price(cmp) > player_money)
+                .iter().all(|(_, &too_expensive)| too_expensive);
+
+            if cant_afford {
+                let msg = "You can't afford any stock!".to_owned().into_boxed_str();
+                self.chat_panel.add_message(msg);
+                return Some(ClientMessage::TakingTurn(
+                    PlayerAction::BuyStock { stock: [None; 3] }))
+            }
+        }
+        None
+    }
+
+    fn process_tile_draw_message(&mut self, tile: Tile) {
+
+        // Assumes a game is in progress
+        let game = self.game.game().unwrap();
+
+        // Assumes the player's hand isn't already full
+        self.action_panel.provide_tile(tile, game.board()).unwrap();
+
+        let msg = format!("You drew tile {}.", tile).into_boxed_str();
+        self.chat_panel.add_message(msg);
+    }
+
 
     /// Call this function any time the size of the terminal changes. This
     /// resizes each sub-panel and re-renders everything.
